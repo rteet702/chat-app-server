11cb5b7e0f70ffa2a8216f828ac0216e0c334092
Robert Teets <teetsrobert@gmail.com>
2022-11-09 19:15:46 -0700

    chore: take three

--- server.js
+++ server.js
@@ -8,18 +8,19 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 
-app.use(
-    cors({
-        credentials: true,
-        origin: `https://chatapp-teets-dev.herokuapp.com/`,
-    })
-);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 require("./routes/test.routes")(app);
 
-const io = new Server(server);
+const io = new Server(server, {
+    cors: {
+        origin: "https://chatapp-teets-dev.herokuapp.com/",
+        methods: ["GET", "POST"],
+        credentials: true,
+    },
+});
 io.on("connection", (socket) => {
     console.log("a user connected.");
 
