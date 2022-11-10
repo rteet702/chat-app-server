74c07e8d4297d7c6e2d9f06f921ad2b7104ae805
Robert Teets <teetsrobert@gmail.com>
2022-11-09 18:57:20 -0700

    feat: added basic socket.io test

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const http = require("http");
+const { Server } = require("socket.io");
+
 const PORT = process.env.PORT || 8000;
 
 const app = express();
+const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
@@ -10,6 +14,19 @@ app.use(express.urlencoded({ extended: true }));
 
 require("./routes/test.routes")(app);
 
-app.listen(PORT, () => {
+const io = new Server(server);
+io.on("connection", (socket) => {
+    console.log("a user connected.");
+
+    socket.on("disconnect", () => {
+        console.log("a user disconnected.");
+    });
+
+    socket.on("test", () => {
+        io.emit("all_user_test", { property: "Hi!" });
+    });
+});
+
+server.listen(PORT, () => {
     console.log("Listening on port " + PORT);
 });
