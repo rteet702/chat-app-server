b42ddd49610e15d4cf09de65e821f5164017d838
Robert Teets <teetsrobert@gmail.com>
2022-11-09 19:10:19 -0700

    feat: configured cors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors());
+app.use(cors({ credentials: true, origin: `http://localhost:${PORT}` }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
